fb34205a26f995a47ac0370c594d18a6a0fb920e
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-10-29 11:45:13 +0700

    IMPLEMENT: color bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+// import color bloc
+import 'package:bloc_native/color_bloc.dart';
+// import flutter bloc lib
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: BlocProvider<ColorBloc>(
+        builder: (context) => ColorBloc(),
+        child: MainPage(),
+      ),
     );
   }
 }
 
 class MainPage extends StatelessWidget {
-  String _title = 'BloC Lib';
+  final String _title = 'BloC Lib';
   @override
   Widget build(BuildContext context) {
+    ColorBloc colorBloc = BlocProvider.of<ColorBloc>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(_title),
@@ -23,26 +31,32 @@ class MainPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              colorBloc.dispatch(ColorEvent.to_amber);
+            },
             backgroundColor: Colors.amber,
           ),
           SizedBox(
             width: 10,
           ),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              colorBloc.dispatch(ColorEvent.to_light_blue);
+            },
             backgroundColor: Colors.lightBlue,
           ),
         ],
       ),
       body: Center(
-        child: AnimatedContainer(
-          decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.all(Radius.circular(10))),
-          duration: Duration(milliseconds: 500),
-          width: 100,
-          height: 100,
+        child: BlocBuilder<ColorBloc, Color>(
+          builder: (context, currentColor) => AnimatedContainer(
+            decoration: BoxDecoration(
+                color: currentColor,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            duration: Duration(milliseconds: 500),
+            width: 100,
+            height: 100,
+          ),
         ),
       ),
     );
