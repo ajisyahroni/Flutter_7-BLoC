ce8eadb0eb08845f5eb72a8ea0637e094e831ff5
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-10-29 11:31:44 +0700

    INIT: init main.dart for bloc lib

--- lib/main.dart
+++ lib/main.dart
@@ -2,47 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _title = 'BloC from Scracth';
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(_title),
-        ),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {},
-              backgroundColor: Colors.amber,
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            FloatingActionButton(
-              onPressed: () {},
-              backgroundColor: Colors.lightBlue,
-            ),
-          ],
-        ),
-        body: Center(
-          child: AnimatedContainer(
-            decoration: BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.all(Radius.circular(10))),
-            duration: Duration(milliseconds: 500),
-            width: 100,
-            height: 100,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  String _title = 'BloC Lib';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.amber,
           ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.lightBlue,
+          ),
+        ],
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          duration: Duration(milliseconds: 500),
+          width: 100,
+          height: 100,
         ),
       ),
     );
